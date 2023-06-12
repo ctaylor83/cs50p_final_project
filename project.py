3adb890766da31715c16915f169b2a3e0e2dedcb
Chris Taylor <ctaylor83@gmail.com>
2023-06-12 15:18:30 +0100

    Creation of basic structure and files

---
+++ project.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def main():
+    app.run(Debug=True)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
