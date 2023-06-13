ddc288876324d46155d732fbb98eacbbaaa91229
Chris Taylor <ctaylor83@gmail.com>
2023-06-13 12:52:09 +0100

    Update of code to move api to separate file

--- project.py
+++ project.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
+
 def main(): # The main function will start the flask server
-    app.run(Debug=True)
+    app.run(debug=True)
 
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
-    if requests.method == 'POST': # This checks if the user has made the request via the web page 'index.html'
+    if request.method == 'POST': # This checks if the user has made the request via the web page 'index.html'
         city = request.form['city'] # Pulls out the city information input by the user & adds to the url for the api request
         units = request.form['units'] # This adds the unit information selected by the user via the dropdown on the web page & adds to the url for the api request
         weather_data = get_weather(city, units)
@@ -18,8 +18,9 @@ def index():
     else:
         return render_template('index.html')
 
-def get_weather(city: str, units: str = 'metric'):
-    API_KEY = 'e31314141ffd3f282f73a356d49fb238'
+def get_weather(city: str, units: str = 'metric'): # Note that metric is selected by default if ever the user did not select a unit of measurement
+    with open('openweatherapi.txt', 'r') as file: # Calling the open weather api key text file
+        API_KEY = file.read().strip() # Reads the API key from the file
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units={units}'
     response = requests.get(url)
     return response.json()
@@ -28,7 +29,7 @@ def get_temperature(weather_data: dict):
     return weather_data['main']['temp']
 
 def get_wind_speed(weather_data: dict):
-    return weather_data['wind']['speed']
+    return weather_data['wind']['speed'] 
 
 if __name__ == '__main__':
     main()
