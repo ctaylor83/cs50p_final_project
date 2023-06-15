e214a2624d9ffb72695f562298d07cf5739e3318
Chris Taylor <ctaylor83@gmail.com>
2023-06-15 15:06:14 +0100

    Updates to files, need to work on errors

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 import requests
 
 app = Flask(__name__)
@@ -11,13 +11,16 @@ def index():
         city = request.form['city'] # Pulls out the city information input by the user & adds to the url for the api request
         units = request.form['units'] # This adds the unit information selected by the user via the dropdown on the web page & adds to the url for the api request
         weather_data = get_weather(city, units)
-        if weather_data.get('message'):  # Check if there's an error message in the response
-            flash(weather_data['message'], 'error')
+
+        if weather_data is None:  # Check if there's an error message in the response
+            flash('City not found, please try again') # Warn the user that they have selected an incorrect city
             return redirect(url_for('index'))  # Redirect back to index page
-        temperature = get_temperature(weather_data)
-        wind_speed = get_wind_speed(weather_data)
-        weather_description, weather_icon = get_weather_info(weather_data)
-        return render_template('index.html', temperature=temperature, wind_speed=wind_speed, city=city, weather_description=weather_description, weather_icon=weather_icon)
+        
+        else:
+            temperature = get_temperature(weather_data)
+            wind_speed = get_wind_speed(weather_data)
+            weather_description, weather_icon = get_weather_info(weather_data)
+            return render_template('index.html', temperature=temperature, wind_speed=wind_speed, city=city, weather_description=weather_description, weather_icon=weather_icon)
     else:
         return render_template('index.html')
 
@@ -30,11 +33,6 @@ def get_weather(city: str, units: str = 'metric'): # Note that metric is selecte
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units={units}'
     response = requests.get(url)
     data = response.json()
-
-    if response.status_code != 200: # If the user inputs an incorrect city this error should flag
-        flash(data.get('message', 'Invalid city, unable to get weather data.')) # Flash the error message from the API or a generic error message
-        return None
-
     return data
 
 def get_temperature(weather_data: dict): # extracts the weather info from the weather data dictionary
@@ -48,4 +46,5 @@ def get_weather_info(weather_data: dict):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'cs50pyweather' # Secret key set to enable Flask flash function
     main()
